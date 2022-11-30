20edc67e3908151a39bfdfbd6628e2c6b0c636c3
Sanket Tarsariya <sanket.tarsariya@ordextechnology.com>
2022-11-30 18:03:11 +0530

    update DataContext file methods

--- lib/data_context/data_context.dart
+++ lib/data_context/data_context.dart
@@ -8,7 +8,7 @@ abstract class DataContext<Key> {
   Future<Key?> add({required String tableName, required Map<String, dynamic> data});
   void remove({required String tableName, required String key});
   Future<List<dynamic>> readWhereContain({required String tableName, Map<String, String>? constraint});
-  Future<bool> addNewData({required String tableName, required Map<String, dynamic> data});
+
   Future<List<dynamic>> readAllData({required String tableName,required bool archive,required int pageNumber});
   Future<List<dynamic>> readDataByKey({required String tableName, required Key key});
 }
